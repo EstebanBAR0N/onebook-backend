001d7f5dccf75e745dbb0260bf0853cdc8b546ea
esteban <esteban.baron@etu.umontpellier.fr>
2022-02-04 08:42:49 +0100

    Add limit to 50mb

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const logRoutes = require('./routes/log');
 const app = express();
 
 // body-parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // cors policy
 app.use(cors());
